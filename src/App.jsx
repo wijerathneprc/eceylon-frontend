dc09b4d1ffee842abf804ff87e93530689602b11
Rishan Wijerathne <wijerathneprc92@gmail.com>
2025-11-18 20:38:48 +0530

    add LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import {Routes, Route} from 'react-router';
 import './App.css'
 import { HomePage } from './pages/HomePage';
+import { LoginPage} from './pages/LoginPage'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <Routes>
 
       <Route path='/' element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
     </Routes>
       
     </>
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,30 +2,30 @@
 
 import './LoginPage.css';
 
-export function LoginPage(){
-    return(
+export function LoginPage() {
+    return (
 
         <>
-        <div className="login-container">
-        <h1><a className="logo-link-h1" href="index.html">eCEYLON</a></h1>
+            <div className="login-container">
+                <h1><a className="logo-link-h1" href="index.html">eCEYLON</a></h1>
 
-        <form className="login-form" action="/login" method="post">
-            <h2> Login to your account </h2>
-            <input type="text" id="username" name="username" required placeholder="Username" />
-            <input type="password" id="password" name="password" required placeholder="Password" />
-            <button class="login-btn" type="submit">Login</button>
-            <div class="login-remember-me">
-                <input type="checkbox" id="remember-me" name="remember-me"/>
-                <label for="remember-me"> Remember me </label>
+                <form className="login-form" action="/login" method="post">
+                    <h2> Login to your account </h2>
+                    <input type="text" id="username" name="username" required placeholder="Username" />
+                    <input type="password" id="password" name="password" required placeholder="Password" />
+                    <button class="login-btn" type="submit">Login</button>
+                    <div class="login-remember-me">
+                        <input type="checkbox" id="remember-me" name="remember-me" />
+                        <label for="remember-me"> Remember me </label>
+                    </div>
+                </form>
+
+                <div className="login-register-pasword">
+                    <a className="register-btn" href="register.html"> Register </a>
+                    <a className="forget-password-btn" href="#"> forget password? </a>
+                </div>
             </div>
-        </form>
 
-        <div className="login-register-pasword">
-            <a className="register-btn" href="register.html"> Register </a>
-            <a className="forget-password-btn" href="#"> forget password? </a>
-        </div>
-    </div>
-        
         </>
     )
 }
