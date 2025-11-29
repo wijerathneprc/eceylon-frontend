d4c65160e2549ea51c2e3f00a6de95daefd9f8db
Rishan Wijerathne <wijerathneprc92@gmail.com>
2025-11-29 14:02:48 +0530

    add style to register-page

--- src/pages/register_page/RegisterPage.jsx
+++ src/pages/register_page/RegisterPage.jsx
@@ -1,44 +1,42 @@
-
-import { Link } from 'react-router';
-import './RegisterPage.css';
 import { useState } from 'react';
 
+import { Link } from 'react-router';
 import axios from 'axios';
 import FormData from 'form-data';
 
-import { Button } from '../../components/button/Button'
+import { Loading } from '../../components/loading/Loading'
+
+import './RegisterPage.css';
+
 
 export function RegisterPage() {
 
+    const [loading, setLoading] = useState(false)
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [rePassword, setRePassword] = useState('')
+    const [retypePasswordMessage, setRetypePasswordMessage] = useState('')
 
     const formData = new FormData()
 
-    const handleFirstName = (event) => {
-        setFirstName(event.target.value)
-    }
-    const handleLastName = (event) => {
-        setLastName(event.target.value)
-    }
-    const handleEmail = event => {
-        setEmail(event.target.value)
-    }
+    const handleFirstName = (event) => { setFirstName(event.target.value) }
 
-    const handlePassword = (event) => {
-        setPassword(event.target.value)
-    }
+    const handleLastName = (event) => { setLastName(event.target.value) }
+
+    const handleEmail = event => { setEmail(event.target.value) }
+
+    const handlePassword = (event) => { setPassword(event.target.value) }
 
     const handleRePassword = (event) => {
         setRePassword(event.target.value)
         const checkPassword = password.startsWith(event.target.value)
-        console.log(checkPassword)
+        checkPassword ? setRetypePasswordMessage(null) : setRetypePasswordMessage('Miss match password')
     }
 
     const handleSubmit = async () => {
+        setLoading(true)
         formData.append('username', email)
         formData.append('first_name', firstName)
         formData.append('last_name', lastName)
@@ -46,6 +44,7 @@ export function RegisterPage() {
         formData.append('password', password)
         const response = await axios.post('http://127.0.0.1:8000/estore/user/', formData)
         console.log(response.data)
+        setLoading(false)
     }
 
     return (<>
@@ -60,18 +59,29 @@ export function RegisterPage() {
                     </div>
                     <div className='email-address-input'>
                         <input type="email" name='email' placeholder='Email Address' autoComplete="username" onChange={handleEmail} />
+                        {<div className='email-info'></div>}
                     </div>
                     <div className='password-input'>
-                        <input type="password" name='password' placeholder='Password' autoComplete="new-password" onChange={handlePassword} />
-                        <input type="password" name="password" placeholder='Retype Password' autoComplete="new-password" onChange={handleRePassword} />
+                        <div className='first-type-password'>
+                            <input type="password" name='password' placeholder='Password' autoComplete="new-password" onChange={handlePassword} />
+                            {<div className='password-info'></div>}
+
+                        </div>
+                        <div className='retype-password'>
+                            <input type="password" name="password" placeholder='Retype Password' autoComplete="new-password" onChange={handleRePassword} />
+                            {retypePasswordMessage ? <ul className='retype-password-info'><>
+                                <li>{retypePasswordMessage}</li>
+                            </></ul> : null}
+                        </div>
                     </div>
                     <div className='register-btn-container'>
                         <button type="submit" onClick={handleSubmit} className='register-btn'>Register</button>
-                        <Link className='login-link-register' to={'/login'}>Already have an account?</Link>
+                        <Link className='register-pg-login-link' to={'/login'}>Already have an account?</Link>
                     </div>
                 </div>
-            </div>
-        </div>
 
+            </div>
+            {loading ? <Loading /> : null}
+        </div>
     </>)
 }
